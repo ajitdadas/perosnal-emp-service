8085dd5f6e664e0a0869acdcbf9fde4b9f184e13
ajitdadas <ajit.17dadas@gmail.com>
2023-05-28 13:06:28 +0530

    First Commit
    First Commit

---
+++ src/main/java/com/peronal/emp/EmployeePersonalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.peronal.emp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePersonalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePersonalServiceApplication.class, args);
+	}
+
+}
