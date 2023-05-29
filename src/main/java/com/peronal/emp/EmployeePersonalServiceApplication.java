42e9d299fc983bd95b63b79ee2e3ee4d90b18033
ajitdadas <ajit.17dadas@gmail.com>
2023-05-29 08:34:01 +0530

    Trigger Test

--- src/main/java/com/peronal/emp/EmployeePersonalServiceApplication.java
+++ src/main/java/com/peronal/emp/EmployeePersonalServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeePersonalServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePersonalServiceApplication.class, args);
+		SpringApplication.run(EmployeePersonalServiceApplication.class, args);	
 	}
 
 }
